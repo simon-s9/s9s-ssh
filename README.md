21ad6ef281a9a8ba11c122689d019fd29f914b75
Semen Kamenetskiy <skamenetskiy@live.com>
2016-09-09 15:21:23 +0300

    v0.0.2

--- README.md
+++ README.md
@@ -6,3 +6,37 @@ Promise based ssh client
 ```
 npm install --save s9s-ssh
 ```
+
+## Usage
+```javascript
+'use strict';
+
+var Client = require(__dirname + '/../ssh');
+var ssh = new Client();
+
+ssh
+    .connect({
+        host: 'somehost.com',
+        port: 22,
+        username: 'root',
+        privateKey: require('fs').readFileSync('/path/to/id_rsa')
+    })
+    .then(() => {
+        return ssh.exec('uptime');
+    })
+    .then(function (result) {
+        console.log(result);
+    }
+    .then(() => {
+        return ssh.exec('whoami')
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+    .finally(() => {
+        ssh.end();
+    });
+```
--- ssh.js
+++ ssh.js
@@ -2,64 +2,99 @@
 
 exports = module.exports = Client;
 
-const SshClient = require('ssh2').Client;
+const Ssh2Client = require('ssh2').Client;
 const Promise = require('promise');
 
 /**
- * Wrapper constructor
+ * Client
+ * @param {Object} config
+ * @param {Object} logger
  * @constructor
  */
-function Client() {
-    this.client = new SshClient();
+function Client(config, logger) {
+    this.config = config || {};
+    this.logger = logger || console;
+    this.client = new Ssh2Client();
 }
 
 /**
- * Opens an ssh connection
+ * Connect to SSH using config
+ * @param {Object} [config]
  * @returns {Promise}
  */
-Client.prototype.connect = function (params) {
-    return new Promise((resolve) => {
-        this.client
-            .on('ready', resolve)
-            .connect(params);
+Client.prototype.connect = function (config) {
+    return new Promise((resolve, reject) => {
+        const connectionConfig = config || this.config || {};
+        try {
+            this.client
+                .on('ready', () => {
+                    this.logger.info(`Connected to ${connectionConfig.host}`);
+                    resolve();
+                })
+                // .on('end', reject)
+                // .on('error', reject)
+                // .on('close', reject)
+                .connect(connectionConfig);
+        } catch (error) {
+            return reject(error);
+        }
     });
 };
 
 /**
- * Executes ssh command
- * @param {string} command
+ * Execute an SSH command
+ * @param {String} command
+ * @param {Object} [options]
  * @returns {Promise}
  */
-Client.prototype.exec = function (command) {
+Client.prototype.exec = function (command, options) {
+    options = options || {};
     return new Promise((resolve, reject) => {
         this.client
-            .exec(command, (error, stream) => {
+            .exec(command, options, (error, stream) => {
                 if (error) {
                     return reject(error);
-                } else {
-                    var data = '';
-                    stream
-                        .on('close', (code, signal) => {
-                            reject(new Error('Connection closed with code: ' + code + ', signal:' + signal));
-                        })
-                        .on('data', (chunk) => {
-                            data += chunk;
-                        })
-                        .on('end', () => {
-                            resolve(data);
-                        })
-                        .stderr
-                        .on('data', (error) => {
-                            reject(new Error(error));
-                        });
                 }
+                let data = '';
+                stream
+                    .on('close', (code, signal) => {
+                        if (code !== 0) {
+                            const message = `Stream::close (code: ${code}, signal: ${signal}, data: ${data.trim()})`;
+                            const error = new Error(message);
+                            error.code = code;
+                            error.signal = signal;
+                            error.data = data;
+                            this.logger.error(error);
+                            return reject(error);
+                        }
+                        return resolve(data);
+                    })
+                    .on('data', (chunk) => {
+                        data += chunk;
+                    })
+                    .stderr
+                    .on('data', (chunk) => {
+                        data += chunk;
+                    });
             });
     });
 };
 
 /**
- * Closes the ssh connection
+ * Add a client event
+ * @param eventName
+ * @param eventCallback
+ * @returns {Client}
+ */
+Client.prototype.on = function (eventName, eventCallback) {
+    this.client.on(eventName, eventCallback);
+    return this;
+};
+
+/**
+ * End the connection
  */
 Client.prototype.end = function () {
     this.client.end();
+    return this;
 };
